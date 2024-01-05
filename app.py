19985eb1eca0f8396638123905ee56bd7ecf743e
leolanese <laneseleo@gmail.com>
2024-01-05 10:27:34 +0000

    updating python debugger and add button to API det Request template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request, render_template
+
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/intro')
+@app.route('/api/intro', methods=['GET'])
 def intro():
     data = {
         'message': 'Python server API message',
@@ -16,5 +17,27 @@ def intro():
     }
     return jsonify(data)
 
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    data = [
+        {
+            'id': 1,
+            'title': u'Buy groceries',
+            'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+            'done': False
+        },
+        {
+            'id': 2,
+            'title': u'Learn Python',
+            'description': u'Need to find a good Python tutorial on the web',
+            'done': False
+        }
+    ]
+    return jsonify(data)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': '404 - Not found'}), 404)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
