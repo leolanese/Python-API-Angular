6fb2d77f516b45d1d6b6ab45404fa4ff696ce20d
leolanese <laneseleo@gmail.com>
2024-01-09 10:40:36 +0000

    cleanning selenium API test

--- test/test_local_selenium.py
+++ test/test_local_selenium.py
@@ -2,16 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 url = "http://127.0.0.1:8000/"
+expected_text = "API Server root - http://localhost:8000/"
 
 driver = webdriver.Chrome()
 driver.get(url)
 
 body_element = driver.find_element(By.TAG_NAME, "body")
 
-if "API Server root - http://localhost:8000/" in body_element.text:
-    print("Text found on the page.")
+if expected_text in body_element.text:
+    print(f"Success: Text '{expected_text}' found on the page.")
 else:
-    print("Text not found on the page.")
+    print(f"Error: Text '{expected_text}' not found on the page.")
 
-# Close the browser
 driver.quit()
