276ab515115c69aa0ecd94b6206ffe7e10ae42b0
leolanese <laneseleo@gmail.com>
2024-01-09 10:37:58 +0000

    including selenium test for API

--- test/test_local_selenium.py
+++ test/test_local_selenium.py
@@ -3,14 +3,11 @@ from selenium.webdriver.common.by import By
 
 url = "http://127.0.0.1:8000/"
 
-# Use a webdriver (make sure to have the appropriate driver installed, e.g., chromedriver)
 driver = webdriver.Chrome()
 driver.get(url)
 
-# Find the text inside the body tag
 body_element = driver.find_element(By.TAG_NAME, "body")
 
-# Check if the desired text is present in the body
 if "API Server root - http://localhost:8000/" in body_element.text:
     print("Text found on the page.")
 else:
